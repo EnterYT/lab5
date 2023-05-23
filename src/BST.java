f115a3420839af1330faecc1ed137027ac0f5bd0
EnterYT <toleu.era10.2004@gmail.com>
2023-05-23 09:51:58 +0600

    Binary tree repository

--- src/BST.java
+++ src/BST.java
@@ -10,8 +10,17 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
+    BST(){
+        root = null;
+    }
     public void put(K key, V val){
-
+        Node child = new Node(key, val);
+        if ((int)root.val >= (int)child.val){
+            root.left = child;
+        }
+        if ((int)root.val < (int)child.val){
+            root.right = child;
+        }
     }
 
     public V get(K key){
