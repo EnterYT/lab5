2af56a49a6b2a4b0f4077be5fe53bc8de1f391dd
EnterYT <toleu.era10.2004@gmail.com>
2023-05-25 21:38:48 +0600

    Binary tree repository

--- src/BST.java
+++ src/BST.java
@@ -46,11 +46,40 @@ public class BST<K extends Comparable<K>, V>{
             return node;
     }
 
-    public void delete(K key){
+    public void delete(K key) {
         if (key == null)
             throw new IllegalArgumentException("Null key is not allowed.");
 
         root = delete(root, key);
+    }
+
+    private Node<K, V> delete(Node<K, V> node, K key) {
+        if (node == null)
+            return null;
+
+        int comparison = key.compareTo(node.key);
+
+        if (comparison < 0)
+            node.left = delete(node.left, key);
+        else if (comparison > 0)
+            node.right = delete(node.right, key);
+        else {
+            if (node.left == null && node.right == null) {
+                return null;
+            }
+            else if (node.left == null)
+                return node.right;
+            else if (node.right == null)
+                return node.left;
+            else {
+                Node<K, V> replaceTemp = findMin(node.right);
+                node.key = replaceTemp.key;
+                node.val = replaceTemp.val;
+                node.right = deleteMin(node.right);
+            }
+        }
+
+        return node;
     }
 
     public Iterable<K> iterator(){
